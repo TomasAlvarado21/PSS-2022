f6f9f96679491846f531ed9a4a2189fcfc8b89e4
Tomas Alvarado <55769898+TomasAlvarado21@users.noreply.github.com>
2022-04-10 20:19:46 -0400

    punteros

---
+++ Punteros/malloc-leak.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *copia(char *str) {
+  char *res = malloc(strlen(str)+1);
+  strcpy(res, str);
+  return res;
+}
+
+int main() {
+  char *r= copia("hola");
+  r[0]= 'H';
+  printf("%s\n", r);
+  // printf("%s\n", "chao");
+  return 0;
+}
