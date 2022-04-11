3ba8e9fc6f21d058698dec77fa1c39065f8fab82
Tomas Alvarado <55769898+TomasAlvarado21@users.noreply.github.com>
2022-04-11 15:49:25 -0400

    casi redi

---
+++ T2v2/elim.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "elim.h"
+
+void eliminar_espacios(char *s) {
+  int contador = 0;
+  char *r = s;
+  char *p = s;
+  int i;
+
+  for (i = 0; i < strlen(s)+1; i++){
+    if(*p == '\0'){
+      *r = *p;
+      break;
+    }
+    else if(*p == ' ' && contador > 0){
+      p++;
+      contador++;
+    }
+    else if (*p == ' ' && contador == 0){
+      *r = *p;
+      contador++;
+      p++;
+      r++;
+    }
+    else{
+      *r = *p;
+      p++;
+      r++;
+      contador=0;
+    }
+  }
+}
+char *eliminacion_espacios(const char *s) {
+  char f[strlen(s)+1];
+  strcpy(f,s);
+  eliminar_espacios(f);
+  char *a;
+  a = malloc(strlen(f)+1);
+  strcpy(a,f);
+  return a;
+}
