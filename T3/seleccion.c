c214cb41c9b1e63b3c27c18ce05473d1e1266d2b
Tomas Alvarado <55769898+TomasAlvarado21@users.noreply.github.com>
2022-04-25 15:54:50 -0400

    tamo

--- T3/seleccion.c
+++ T3/seleccion.c
@@ -27,17 +27,24 @@ int selprim(Nodo **pa, int k) {
 Nodo *ultimos(Nodo *a, int *pk) {
   int contador_der = *pk; 
   
-  if(a == NULL) return 0;
+  if(a == NULL){
+    *pk = 0;
+    return 0;
+  };
   
-  Nodo *right = ultimos(a->der, *pk);
-  if(*contador_der == *pk){
+  Nodo *right = ultimos(a->der, &contador_der);
+  if(contador_der == *pk){
     return right;
   }
   int contador_izq = *pk-contador_der-1;
-  Nodo *left = ultimos(a->izq, *pk-contador_der-1);
+
+  Nodo *left = ultimos(a->izq, &contador_izq);
+  
   Nodo *b = malloc(sizeof(Nodo));
+  b->x = a->x;
   b->der = right;
   b->izq = left;
 
+  *pk = contador_der+1+contador_izq;
   return b;
 }
