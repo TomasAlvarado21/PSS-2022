47d3cca6216456e515ac8a034448980c16f27822
Tomas Alvarado <55769898+TomasAlvarado21@users.noreply.github.com>
2022-06-06 04:01:15 -0400

    a

--- T5/sort-c-esp.c
+++ T5/sort-c-esp.c
@@ -4,15 +4,37 @@ void sort(char **noms, int n) {
   char **ult= &noms[n-1];
   char **p= noms;
   while (p<ult) {
-    ////////////////////////////////////////////
-    // Comienza el codigo que Ud. debe modificar
-    ////////////////////////////////////////////
- 
-    int rc= strcmp(p[0], p[1]);
+    char *p1=[0];
+    char *p2=[1];
+    int c1 = 0;
+    int c2 = 0;
+    while(*p1 !='/0'){
+      while (*p1 != ' '){
+        p1++;
+      }
+      p1++;
+      c1++;   
+    }
 
-    ////////////////////////////////////////////
-    // Fin del codigo que Ud. debe modificar
-    ////////////////////////////////////////////
+    while(*p2 !='/0'){
+      while (*p2 != ' '){
+        p2++;
+      }
+      p2++;
+      c2++;      
+    }
+    
+    if(c1>c2){
+      int rc = 1;
+    }
+    else(){
+        int rc = -1;
+    }
+    
+    
+    //int rc= strcmp(p[0], p[1]);
+    
+    
     if (rc<=0)
       p++;
     else {
