2fc4c01cbddcd6e9dfcc085fd46c22d0735d6c40
Tomas Alvarado <55769898+TomasAlvarado21@users.noreply.github.com>
2022-07-05 14:08:51 -0400

    esooo

--- T7/fav.c
+++ T7/fav.c
@@ -25,21 +25,35 @@ int favorables(int n, int sum, int rep) {
 // the parameter rep simulates the throwing rep times the n dices, returning the cases that the sum of the results is equals to sum
 // we need to parallelize this function to estimate the probability of throwing n dices , the sum of the results is equals to sum using p cores
 // for that we need to invoke p times fork() to create p processes, each process will throw n dices and will sum the results, the sum of the results is equals to sum
-
+// p is the number of cores, we need to create p pipes, each pipe will be used by one process to communicate with the other processes
 int favorables_par(int n, int sum, int rep, int p){
   int fav= 0; // número de casos favorables
-  int pid;
-  int status;
-  int i;
-  for (i= 0; i<p; i++) {
-    pid= fork();
-    if (pid==0) {
-      fav+= favorables(n, sum, rep);
-      exit(0);
+  int fds[p][2]; // pipes
+  int pids[p]; // pids
+  int tam_bloque = (rep)/p;
+  
+  for(int i=0; i<p; i++){
+    pipe(fds[i]);
+    pids[i] = fork();
+    if(pids[i] == 0){ // hijo
+    close(fds[i][0]);
+    int izq = tam_bloque*i;
+    int der = tam_bloque*(i+1) - 1;
+    if(i == p-1) der = rep-1;
+    int res = favorables(n, sum, der-izq+1);
+    write(fds[i][1], &res, sizeof(int));
+    exit(0);
     }
-    else {
-      wait(&status);
+    else{ // padre
+    close(fds[i][1]);
     }
+  }
+  
+  for(int i=0; i<p; i++){
+    waitpid(pids[i], NULL, 0);
+    int res_hijo;
+    read(fds[i][0], &res_hijo, sizeof(int));
+    fav += res_hijo;
   }
   return fav;
 }
---
+++ T7/p2.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int N_CORES;
+
+typedef unsigned long long ull;
+typedef unsigned int uint;
+ull buscar_factor(ull x, ull l, ull r){
+	for(ull k = l; k <= r; k++){
+		if(x % k == 0){
+			return k; // k divide a x
+		}
+	}
+	return 0; // no se encontr ́o divisor en [l, r]
+}
+
+ull buscar_factor_par(ull x, ull l, ull r){
+
+	int pids[N_CORES];
+	int fds[N_CORES][2];
+
+	ull tam_bloque = (r-l+1)/N_CORES;
+	
+	for(int i=0; i<N_CORES; i++){
+		pipe(fds[i]);
+		pids[i] = fork();
+
+		if(pids[i] == 0){ // hijo
+			close(fds[i][0]);
+			ull izq = l+tam_bloque*i;
+			ull der = l+tam_bloque*(i+1) - 1;
+			if(i == N_CORES-1) der = r;
+			ull res = buscar_factor(x, izq, der);
+			write(fds[i][1], &res, sizeof(ull));
+			exit(0);
+		}
+		else{ // padre
+			close(fds[i][1]);
+		}
+	}
+
+	ull res = 0;
+	for(int i=0; i<N_CORES; i++){
+		waitpid(pids[i], NULL, 0);
+		ull res_hijo;
+		read(fds[i][0], &res_hijo, sizeof(ull));
+		if(res_hijo != 0){
+			res = res_hijo;
+		}
+	}
+	return res;
+}
+
+
+int main(int argc, char* argv[]){
+	N_CORES = atoi(argv[1]);
+
+	
+	const ull PRIMO = 1e9+7;
+
+	ull res = buscar_factor_par(67543411, 2, PRIMO-1);
+
+	printf("Divisor: %d\n", res);
+
+	return 0;
+}
